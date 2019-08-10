4d8a09b9944cace3c916ece190dd7d91488ce89c
Sergey Shcherbakov <sevlshcher@gmail.com>
2019-08-10 23:47:12 +0300

    did auth.js, test passed

--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -2,10 +2,41 @@ import React, { PureComponent } from 'react';
 
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
 
+const args = {email: 'stu@dent.com', password: '123'}
+
 class AuthProvider extends PureComponent {
+  state = {email:'', authorizeError:'', isAuthorized: false}
+
+  authorize = (email, password) => {
+    if (email === args.email && password === args.password) {
+      this.setState({
+        isAuthorized: true,
+        email,
+        authorizeError:''
+      })
+    } else {
+      this.setState({authorizeError: 'Email или пароль введён не верно'})
+    }
+  }
+  
+  logout = () => {
+    this.setState({isAuthorized: false})
+  }
+
+  getProviderValue = () => {
+    const {email, isAuthorized, authorizeError} = this.state
+
+    return {
+      email,
+      isAuthorized,
+      authorizeError,
+      authorize: this.authorize,
+      logout: this.logout
+    }
+  }
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+    return <Provider value={this.getProviderValue()}>{children}</Provider>;
   }
 }
 
