9005454d301670e642668398dbe7ea8455db074e
Sergey Shcherbakov <sevlshcher@gmail.com>
2019-08-11 04:12:16 +0300

    homework 4 completed, all tests passed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,20 @@
 import React, { PureComponent } from 'react';
 import './Header.css';
+import {AuthConsumer} from '../../contexts/Auth'
 
 class Header extends PureComponent {
   render() {
-    return ('empty');
+    return (
+      <AuthConsumer>
+        {({isAuthorized, email, logout}) => isAuthorized ? (
+          <div className="header-menu">
+            <p className="header-menu__email header-email t-header-email">{email}</p>
+            <button className="header-menu__button t-logout button" onClick={logout}>Выйти</button>
+          </div>
+        ) : null
+        }
+      </AuthConsumer>
+    );
   }
 }
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,55 @@
 import React, { PureComponent } from 'react';
 import './Layout.css';
+import SectionTitle from '../SectionTitle'
 
 class Layout extends PureComponent {
+  get renderHeader() {
+    const {header: Header} = this.props
+    return (
+      Header && (
+        <header className="header">
+          <SectionTitle className="header__title">Header</SectionTitle>
+          <div className="header__content">
+            <Header />
+          </div>
+        </header>
+      )
+    )
+  }
+
+  get renderFooter() {
+    const {footer: Footer} = this.props
+    return (
+      Footer && (
+        <footer className="footer">
+          <SectionTitle className="header__title">Footer</SectionTitle>
+          <Footer />
+        </footer>
+      )
+    )
+  }
+
+  get renderMain() {
+    const {header, footer, children} = this.props
+    return (
+      <main className={`main ${header ? 'main--with-header' : ''} ${footer ? 'main--with-footer' : ''}`}>
+        <SectionTitle className="main__title">Main</SectionTitle>
+        {children}
+      </main>
+    )
+  }
+
   render() {
-    return 'empty';
+    const {children} = this.props
+    return (
+      <div>
+        <React.Fragment>
+          {this.renderHeader}
+          {this.renderMain}
+          {this.renderFooter}
+        </React.Fragment>
+      </div>
+    )
   }
 }
 
